b3ad5935fffb30b46385adabf3b60f0e6058c38a
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2019-11-18 18:24:37 +0100

    First commit
    Generate a password with this Python algorithm

---
+++ main.py
@@ -0,0 +1,44 @@
+import random
+import array
+
+# maximum length of password needed
+MAX_LEN = int(input('Enter the length of your password\n'))
+
+
+# declare arrays of the character that we need in out password
+# Represented as chars to enable easy string concatenation
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                     'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+                     'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                     'z']
+
+UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                     'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q',
+                     'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                     'Z']
+
+SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
+           '*', '(', ')', '<&#039;']
+
+# combines all the character arrays above to form one array
+COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
+
+# randomly select at least one character from each character set above
+rand_digit = random.choice(DIGITS)
+rand_upper = random.choice(UPCASE_CHARACTERS)
+rand_lower = random.choice(LOCASE_CHARACTERS)
+rand_symbol = random.choice(SYMBOLS)
+
+# combine the character randomly selected above
+# at this stage, the password contains only 4 characters
+# we want a 12-character password
+temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol
+
+# we fill the rest of
+# the password length by selecting randomly from the combined
+# list of character above.
+for x in range(MAX_LEN - 4):
+    temp_pass = temp_pass + random.choice(COMBINED_LIST)
+
+print(temp_pass)
